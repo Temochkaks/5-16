fca345c3a3f4d164bb624264a4af07f4fa5e18ee
Temochkaks <artem_ivanenko56@mail.ru>
2022-12-26 13:23:08 +0300

    5 laba

---
+++ create_db.sql
@@ -0,0 +1,11 @@
+/* 
+
+5 laba 
+
+CREATE USER Artemka WITH PASSWORD 'admin'
+
+CREATE DATABASE Ivanenko_the_best WITH OWNER =Artemka ENCODING = 'UTF8';
+
+GRANT ALL PRIVILEGES ON DATABASE Ivanenko_the_best to Artemka;
+*/
+
