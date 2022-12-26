dc55786c06e2cc83905d90597bf7ff3c062a4f43
Temochkaks <artem_ivanenko56@mail.ru>
2022-12-26 13:33:01 +0300

    5 laba

---
+++ data.sql
@@ -0,0 +1,17 @@
+/* 6 laba
+insert into usr (first_name, middle_name, last_name, email) values
+('Стас', 'Романович', 'Иванов', 'adsdasdsda@my.example.com'),
+('Дмитрий', 'Кириллович', 'Петров', 'dadsadad@my.example.com'),
+('Кирилл', 'Станиславович', 'Максимов', 'fhgfhfghf@my.example.com'),
+('Ксения', 'Дмитриевна', 'Ларионова', 'ghjgjghj@my.example.com'),
+('Александра', 'Магаданович', 'Зебестович', 'ityitit@my.example.com'),
+('Елена', 'Маркович', 'Макаронов', 'mityitit@ple.com'),
+('Руслан', 'Романович', 'Апельсинов', 'tjghjgjg@my.example.com'),
+('Рома', 'Иванович', 'Иванов', 'iyuiyiyi@my.example.com'),
+('Анастасия', 'Иванович', 'Круг', 'nbmggh@my.example.com'),
+('Виктория', 'Ивановна', 'Гречай', 'uyoyoyuo@my.example.com'),
+('Никита', 'Маркович', 'Иванов', 'iupuiluil@my.example.com'),
+('Тимофей', 'Романов', 'Иванов', 'uyiyiyiyi@my.example.com'),
+('Александр ', 'Иванович', 'Петров', 'bdbdfdbdf@my.example.com');ф
+*/
+
---
+++ schema_db.sql
@@ -0,0 +1,9 @@
+/* 6 laba 
+create table usr (
+id bigserial not null,
+email varchar(64) not null,
+first_name varchar(64) not null,
+last_name varchar(64) not null,
+middle_name varchar(64) not null);
+);
+*/
